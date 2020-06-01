6fa24d0d13fd24795168e2445c2e2699cdf7da99
Ratna <62145944+noobiehackerboi@users.noreply.github.com>
2020-06-02 02:30:12 +0530

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int q;
+    cin>>q;
+    if(q>2 && q%2==0)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+}
